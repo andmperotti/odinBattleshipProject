f4960b24f38dda302f07ab978d83514998e8bd42
Andrew Perotti <Perotti28@gmail.com>
2025-06-03 09:19:47 -0700

    test: add isSunk tests, omit unnecessary arguments

--- ship.test.js
+++ ship.test.js
@@ -2,17 +2,31 @@ import { Ship } from "./ship.js";
 
 //test public properties of Ship class instances: length of ship, number of times they've been hit, sunk status
 test("length", () => {
-  expect(new Ship(3, 0, false)).toMatchObject({
+  expect(new Ship(3)).toMatchObject({
     length: 3,
     hits: 0,
     sunk: false,
   });
 });
 
-//declare a ship instance for future testing
-let testShip = new Ship(3, 0, false);
-
 //test the hit method, which should increment the number of hits a ship has taken by the argument value
 test("hits method", () => {
+  let testShip = new Ship(3);
   expect(testShip.hit(1)).toMatchObject({ length: 3, hits: 1, sunk: false });
+});
+
+//test the isSunk method, which considers the length of the ship and how many hits it has taken which it either changes the ship to be sunk or I'm guessing logs somewhere that the ship has not sunk
+test("isSunk method", () => {
+  let testShip = new Ship(3);
+  testShip.isSunk();
+  expect(testShip).toMatchObject({ length: 3, hits: 0, sunk: false });
+});
+
+//simulate the ship getting hit three times
+//then test functionality of isSunk again, should be sunk
+test("isSunk method", () => {
+  let testShip = new Ship(3);
+  testShip.hit(3);
+  testShip.isSunk();
+  expect(testShip).toMatchObject({ length: 3, hits: 3, sunk: true });
 });
