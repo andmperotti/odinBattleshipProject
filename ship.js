e8b355e832feebd57d7edea5f87332f5d6c233c0
Andrew Perotti <Perotti28@gmail.com>
2025-06-03 09:05:26 -0700

    feat: add hit method to Ship class

--- ship.js
+++ ship.js
@@ -2,4 +2,8 @@ export class Ship {
   constructor(length, hits, sunk) {
     (this.length = length), (this.hits = hits), (this.sunk = sunk);
   }
+  hit(newHits) {
+    this.hits += newHits;
+    return this;
+  }
 }
