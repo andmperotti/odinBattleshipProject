7ae551c3d85006f93af1569fd65c0ba4c671e63c
Andrew Perotti <Perotti28@gmail.com>
2025-06-05 12:36:45 -0700

    fix: remove argument from hit method

--- ship.js
+++ ship.js
@@ -2,8 +2,8 @@ export class Ship {
   constructor(length, hits = 0, sunk = false) {
     (this.length = length), (this.hits = hits), (this.sunk = sunk);
   }
-  hit(newHits) {
-    this.hits += newHits;
+  hit() {
+    this.hits++;
     this.isSunk();
     return this;
   }
