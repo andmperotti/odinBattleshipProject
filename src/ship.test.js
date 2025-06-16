45064b91f30c338577fdac1af98d23cf8dad3272
Andrew Perotti <Perotti28@gmail.com>
2025-06-16 11:10:39 -0700

    test: omit argument to hit method, call it thrice

--- src/ship.test.js
+++ src/ship.test.js
@@ -1,7 +1,7 @@
 import { describe, expect, test } from "@jest/globals";
 import { Ship } from "./ship.js";
 
-//test public properties of Ship class instances: length of ship, number of times they've been hit, sunk status
+//test public properties of Ship class instances: length of ship, number of times they've been hit, sunk status (should be false)
 test("length", () => {
   expect(new Ship(3)).toMatchObject({
     length: 3,
@@ -13,21 +13,17 @@ test("length", () => {
 //test the hit method, which should increment the number of hits a ship has taken by the argument value
 test("hits method", () => {
   let testShip = new Ship(3);
-  expect(testShip.hit(1)).toMatchObject({ length: 3, hits: 1, sunk: false });
-});
-
-//test the isSunk method, which considers the length of the ship and how many hits it has taken which it either changes the ship to be sunk or I'm guessing logs somewhere that the ship has not sunk
-test("isSunk method", () => {
-  let testShip = new Ship(3);
-  testShip.isSunk();
-  expect(testShip).toMatchObject({ length: 3, hits: 0, sunk: false });
+  testShip.hit();
+  expect(testShip).toMatchObject({ length: 3, hits: 1, sunk: false });
 });
 
 //simulate the ship getting hit three times
 //then test functionality of isSunk again, should be sunk
-test("isSunk method", () => {
+test("ship sinks", () => {
   let testShip = new Ship(3);
-  testShip.hit(3);
-  testShip.isSunk();
+  testShip.hit();
+  testShip.hit();
+  testShip.hit();
+
   expect(testShip).toMatchObject({ length: 3, hits: 3, sunk: true });
 });
