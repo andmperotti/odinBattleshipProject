b51fb5395a9cd7b68fa94b842a47df73c48b0543
Andrew Perotti <Perotti28@gmail.com>
2025-06-16 11:25:22 -0700

    fix: add toLowerCase invocation on type argument

--- src/game.test.js
+++ src/game.test.js
@@ -1,17 +1,25 @@
 import { describe, expect, test } from "@jest/globals";
-import game from "./game.js";
+import { game } from "./game.js";
 
 describe("testing game factory function", () => {
   let testGame = game("drew", "comp", "Computer");
 
+  //   test("return something", () => {
+  //     expect(testGame).toBeTruthy();
+  //     console.log(testGame);
+  //   });
+
   test("first player created", () => {
     expect(testGame.players[0].name).toBe("drew");
+  });
+  test("first player Human type", () => {
+    expect(testGame.players[0].playerInstance.type).toBe("human");
   });
 
   test("second player created", () => {
     expect(testGame.players[1].name).toBe("comp");
   });
-  test("second player type", () => {
-    expect(testGame.players[1].type).toBe("Computer");
+  test("second player Computer type, and changing input to lowercase", () => {
+    expect(testGame.players[1].playerInstance.type).toBe("computer");
   });
 });
