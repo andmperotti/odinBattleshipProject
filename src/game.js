9d66424e1f26f8a1de5e89c71065776ae2c1bb44
Andrew Perotti <Perotti28@gmail.com>
2025-06-16 08:52:59 -0700

    fix: change export to use named exports

--- src/game.js
+++ src/game.js
@@ -1,6 +1,6 @@
 import { Player } from "./player.js";
 
-export default function game(playerOneName, playerTwoName, playerTwoType) {
+function game(playerOneName, playerTwoName, playerTwoType) {
   let players = [
     { name: playerOneName, playerInstance: new Player() },
     { name: playerTwoName, playerInstance: new Player(playerTwoType) },
@@ -8,5 +8,5 @@ export default function game(playerOneName, playerTwoName, playerTwoType) {
 
   return { players };
 }
-
+module.exports = game;
 //Make the game object hold the values for player names and types, make game hold logic for the game actions. Change new game modal to hidden, not removed, that way when player clicks new game button we don't have to remake it
