7e756af44c6854c2ebfa1133cb5ec39bb0a14a6e
Andrew Perotti <Perotti28@gmail.com>
2025-06-17 11:17:09 -0700

    fix: add toLowerCase invocation on type

--- src/player.js
+++ src/player.js
@@ -2,7 +2,7 @@ import { gameboard } from "./gameboard";
 
 export class Player {
   constructor(type = "human") {
-    this.type = type;
+    this.type = type.toLowerCase();
     this.gameboard = gameboard();
   }
 }
