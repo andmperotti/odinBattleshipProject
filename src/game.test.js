94da440a450e257787b104b2861ecd3d68d29cc9
Andrew Perotti <Perotti28@gmail.com>
2025-06-30 10:52:36 -0700

    fix: add playerInstance into expression strings
    This is needed as instances of Player class now keep track of player names, instead of the game factory function.

--- src/game.test.js
+++ src/game.test.js
@@ -5,14 +5,14 @@ describe("testing game factory function", () => {
   let testGame = game("drew", "comp", "Computer");
 
   test("first player created", () => {
-    expect(testGame.players[0].name).toBe("drew");
+    expect(testGame.players[0].playerInstance.name).toBe("drew");
   });
   test("first player Human type", () => {
     expect(testGame.players[0].playerInstance.type).toBe("human");
   });
 
   test("second player created", () => {
-    expect(testGame.players[1].name).toBe("comp");
+    expect(testGame.players[1].playerInstance.name).toBe("comp");
   });
   test("second player Computer type, and changing input to lowercase", () => {
     expect(testGame.players[1].playerInstance.type).toBe("computer");
