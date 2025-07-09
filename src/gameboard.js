4e075127446e2780f46014659120833271212376
Andrew Perotti <Perotti28@gmail.com>
2025-07-09 10:36:42 -0700

    feat: add replaceShip
    This method will allow users when placing their ships to reposition ships.
    
    feat: added type to previously existing methods
    
    type allows us to target specific ships, and is needed for placement and replacement purposes.
    
    feat: add a return statement when a ship cannot be placed
    
    This statement will be shown to the user via pseudo selector below the ship they are trying to place or replace/etc.

--- src/gameboard.js
+++ src/gameboard.js
@@ -46,7 +46,7 @@ export class Gameboard {
     //later will have to add logic for ship to take damage, could instead of using just 0's and 1's, use any number > 0 to correspond to a specific boat
   }
 
-  placeShip(y, x, orientation, length) {
+  placeShip(y, x, orientation, length, type) {
     //check for range of prospective ship length to make sure there is 1. room to fit the ship, and 2. no ship already in the prospective coordinates and length
     if (
       orientation === "up" &&
@@ -54,31 +54,31 @@ export class Gameboard {
       this._withinSeaRange(y + length, x) &&
       this._vacancyChecker(y, x, orientation, length)
     ) {
-      this._addShip(y, x, orientation, length);
+      this._addShip(y, x, orientation, length, type);
     } else if (
       orientation === "down" &&
       this._withinSeaRange(y, x) &&
       this._withinSeaRange(y - length, x) &&
       this._vacancyChecker(y, x, orientation, length)
     ) {
-      this._addShip(y, x, orientation, length);
+      this._addShip(y, x, orientation, length, type);
     } else if (
       orientation === "right" &&
       this._withinSeaRange(y, x) &&
       this._withinSeaRange(y, x + length) &&
       this._vacancyChecker(y, x, orientation, length)
     ) {
-      this._addShip(y, x, orientation, length);
+      this._addShip(y, x, orientation, length, type);
     } else if (
       orientation === "left" &&
       this._withinSeaRange(y, x) &&
       this._withinSeaRange(y, x - length) &&
       this._vacancyChecker(y, x, orientation, length)
     ) {
-      this._addShip(y, x, orientation, length);
+      this._addShip(y, x, orientation, length, type);
     } else {
-      console.log("location occupied");
-      //this is going to be output when program auto places ships randomly
+      //return error message to be displayed on place ships screen under specific element related to error being created, and yes this isn't an error object
+      return "location occupied or out of range";
     }
   }
 
@@ -118,10 +118,10 @@ export class Gameboard {
     }
   }
 
-  _addShip(y, x, orientation, length) {
+  _addShip(y, x, orientation, length, type) {
     //variable to help us keep track of how many spots to take up for length of each ship
     let counter = 0;
-    this._boats.push(new Ship(length));
+    this._boats.push(new Ship(length, type));
     while (counter < length) {
       if (orientation === "up") {
         this.sea[y + counter][x] = this._boats.length;
@@ -141,5 +141,33 @@ export class Gameboard {
 
   boatsRemaining() {
     return this._boats.filter((boat) => !boat.sunk).length;
+  }
+
+  replaceShip(type, y, x, orientation) {
+    let length =
+      type === "carrier"
+        ? 5
+        : type === "battleship"
+          ? 4
+          : type === "destroyer"
+            ? 3
+            : type === "submarine"
+              ? 3
+              : type === "patrol boat"
+                ? 2
+                : null;
+
+    //find the specific boat in the boats array
+    let targetedBoat = this._boats.findIndex((boat) => (boat.type = "type"));
+    //remove it
+    this._boats = this._boats
+      .slice(0, targetedBoat)
+      .concat(this._boats.slice(targetedBoat));
+    //replace it with new boat of same type
+    this.placeShip(y, x, orientation, length, type);
+  }
+
+  clearShips() {
+    this._boats = [];
   }
 }
