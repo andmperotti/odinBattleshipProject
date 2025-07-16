c41377f329d97122ba474178cb3b1cd43b6089e8
Andrew Perotti <Perotti28@gmail.com>
2025-07-16 10:48:36 -0700

    test: change tests to reflect gameBoard changes
    In addition to changing tests to correctly test the changes of gameBoard, I've also moved some tests around to reflect a progression of what would be used from earliest in the app to latest.

--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -16,7 +16,47 @@ test("initialized sea board", () => {
   ]);
 });
 
-test("testSea attack miss", () => {
+test("placing ships", () => {
+  let testSea = new Gameboard();
+  testSea.placeShip(2, 3, "right", 2, "patrol boat");
+  expect(testSea.sea).toMatchObject([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 5, 5, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+  expect(testSea.boats[4]).toMatchObject({ type: "patrol boat" });
+});
+
+test("not placing disallowed ships", () => {
+  //shouldn't place anything
+  let testSea = new Gameboard();
+  testSea.placeShip(5, 6, "down", 1, "yellow submarine");
+  expect(testSea.sea).toMatchObject([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+  expect(
+    testSea.boats.filter((boat) => boat.type === "yellow submarine").length ===
+      0
+  );
+});
+
+test(" attack miss", () => {
   let testSea = new Gameboard();
   testSea.receiveAttack(0, 0);
   expect(testSea.sea).toMatchObject([
@@ -33,55 +73,13 @@ test("testSea attack miss", () => {
   ]);
 });
 
-test("placing ships", () => {
+test("attack hit", () => {
   let testSea = new Gameboard();
-  testSea.placeShip(2, 3, "right", 2);
-  expect(testSea.sea).toMatchObject([
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 1, 1, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  ]);
-  testSea.placeShip(5, 6, "down", 1);
-  expect(testSea.sea).toMatchObject([
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 1, 1, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 2, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  ]);
-});
-
-test("receive attack", () => {
-  let testSea = new Gameboard();
-  testSea.placeShip(0, 0, "right", 3);
-  expect(testSea.sea).toMatchObject([
-    [1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  ]);
+  testSea.placeShip(0, 0, "right", 3, "destroyer");
 
   testSea.receiveAttack(0, 0);
   expect(testSea.sea).toMatchObject([
-    ["+", 1, 1, 0, 0, 0, 0, 0, 0, 0],
+    ["+", 3, 3, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -96,15 +94,61 @@ test("receive attack", () => {
 
 test("Ships still remaining", () => {
   let testBoard = new Gameboard();
-  //   expect(testBoard.boats.filter((boat) => !boat.sunk).length).toBe(0);
   expect(testBoard.boatsRemaining()).toBe(0);
-  testBoard.placeShip(0, 0, "right", 1);
-  //   expect(testBoard.boats.filter((boat) => !boat.sunk).length).toBe(1);
+
+  testBoard.placeShip(0, 0, "right", 2, "patrol boat");
   expect(testBoard.boatsRemaining()).toBe(1);
 
-  testBoard.placeShip(5, 5, "right", 1);
-  //   expect(testBoard.boats.filter((boat) => !boat.sunk).length).toBeGreaterThan(
-  //     0
-  //   );
-  expect(testBoard.boatsRemaining()).toBeGreaterThan(0);
+  testBoard.placeShip(5, 5, "right", 3, "submarine");
+  expect(testBoard.boatsRemaining()).toBeGreaterThan(1);
+});
+
+//test clearShips
+test("set up game and a board, check if players can clear ships and therefore also sea", () => {
+  let testBoard = new Gameboard();
+  testBoard.placeShip(0, 0, "right", 4, "battleship");
+  testBoard.clearShips();
+  expect(testBoard.boats.filter((boat) => boat.type).length).toBe(0);
+  expect(testBoard.sea).toMatchObject([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+});
+
+test("placeShip and replaceShip on user sea", () => {
+  let testBoard = new Gameboard();
+  testBoard.placeShip(0, 0, "right", 4, "battleship");
+  expect(testBoard.sea).toMatchObject([
+    [2, 2, 2, 2, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+  testBoard.replaceShip("battleship", 1, 0, "right");
+  expect(testBoard.sea).toMatchObject([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [2, 2, 2, 2, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
 });
