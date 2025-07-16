aa2fefc7776da62dd63b8d4245df93c2f4c68dfd
Andrew Perotti <Perotti28@gmail.com>
2025-07-16 10:45:19 -0700

    refactor: change _boats array initial value
    _boats initial value was an empty array, we needed to change it to have 5 elements so that we can dynamically alter specific elements at any time, this change gives us that ability.
    
    refactor: change sea references
    
    Before this change we referenced "sea" not "_sea", which is wrong because we need to keep in mind that sea is a private property, and sea would be a getter return and not technically used to set values.
    
    fix: change expressions that target dynamic index's of _sea
    
    There are some expressions that when evaluated should access a dynamic index, yet those expressions were incorrect. These can be summarized as when looking for a prospective block of sea spots to place a future ship we need to correctly target the prospective sea spots but the expressions before this change were targeting the wrong prospective sea spots by one square. There were also some instances of targeting the wrong direction, for example when placing a ship in an 'up' or 'down' orientation that  operator used was incorrect in both cases causing the ship to be placed the other orientation. Likewise operators in _vacancyChecker were changed as they were incorrect as well.
    
    refactor: add else condition in placeShips
    
    In the future I'll tell the user why an action of theirs wasn't possible, so this is an initial attempt at creating that message.
    
    refactor: change how boats are added to _boats
    
    This method now uses a switch statement to create Ship's inside the _boats array at specific index's, as mentioned in another change to _boats. This helps set specific types of boats at specific index's so that other methods can access the correct ship. Similarly I've changed how values are added to _sea, which is now related to the index of the ship in _boats which ties together the type of the ship and the value representing it in the seaboards.
    
    refactor: change boatsRemaining to use equality check
    
    With the change to _boats initial values, we now check for a specific value to the sunk property, before it was a check to see if sunk was truthy which does not work now that _boat elements can be empty strings.
    
    refactor: change parameter names for replaceShip
    
    This change was needed because we need to differentiate between the old ship and the new ship.
    
    refactor: change replaceShip logic
    
    This method has been rewritten as it didn't work after changes to _boats, etc. The statement that leads to removing the old boat was not working correctly and that's because I forgot sea is an array of arrays and if i'm going to use map to change it then i need to chain a map within a map so that it runs for each row of arrays in the sea array.
    
    refactor: change clearShips method
    
    This method earlier changed the value the _boats property of an instance of this class pointed to, to an empty array. This now points to an array of 5 empty strings, which is useful to us because of the other changes to this file and methods that work on index's related to types of a ships.

--- src/gameboard.js
+++ src/gameboard.js
@@ -2,7 +2,7 @@ import { Ship } from "./ship.js";
 
 export class Gameboard {
   constructor() {}
-  _boats = [];
+  _boats = Array.from({ length: 5 }, () => "");
 
   get boats() {
     return this._boats;
@@ -30,15 +30,15 @@ export class Gameboard {
   }
 
   receiveAttack(y, x) {
-    let spotValue = this.sea[y][x];
+    let spotValue = this._sea[y][x];
     //for now this logic just checks the coordinates and changes the value accordingly or logs a statement that that position was already attacked
     if (spotValue === 0) {
-      this.sea[y][x] = "-";
+      this._sea[y][x] = "-";
       //miss
       this._misses++;
     } else if (spotValue > 0) {
       this._boats[spotValue - 1].hit();
-      this.sea[y][x] = "+";
+      this._sea[y][x] = "+";
       //hit
     } else {
       console.log("position already attacked, try again");
@@ -51,34 +51,34 @@ export class Gameboard {
     if (
       orientation === "up" &&
       this._withinSeaRange(y, x) &&
-      this._withinSeaRange(y + length, x) &&
+      this._withinSeaRange(y - (length - 1), x) &&
       this._vacancyChecker(y, x, orientation, length)
     ) {
       this._addShip(y, x, orientation, length, type);
     } else if (
       orientation === "down" &&
       this._withinSeaRange(y, x) &&
-      this._withinSeaRange(y - length, x) &&
+      this._withinSeaRange(y + (length - 1), x) &&
       this._vacancyChecker(y, x, orientation, length)
     ) {
       this._addShip(y, x, orientation, length, type);
     } else if (
       orientation === "right" &&
       this._withinSeaRange(y, x) &&
-      this._withinSeaRange(y, x + length) &&
+      this._withinSeaRange(y, x + (length - 1)) &&
       this._vacancyChecker(y, x, orientation, length)
     ) {
       this._addShip(y, x, orientation, length, type);
     } else if (
       orientation === "left" &&
       this._withinSeaRange(y, x) &&
-      this._withinSeaRange(y, x - length) &&
+      this._withinSeaRange(y, x - (length - 1)) &&
       this._vacancyChecker(y, x, orientation, length)
     ) {
       this._addShip(y, x, orientation, length, type);
     } else {
       //return error message to be displayed on place ships screen under specific element related to error being created, and yes this isn't an error object
-      return "location occupied or out of range";
+      return "location occupied or out of range, or type of ship has been placed";
     }
   }
 
@@ -94,19 +94,19 @@ export class Gameboard {
     let turn = 0;
     let wantedSpots = Array.from({ length: length }, () => {
       if (orientation === "up") {
-        let prospectiveSlot = this.sea[y + turn][x];
+        let prospectiveSlot = this._sea[y - turn][x];
         turn++;
         return prospectiveSlot;
       } else if (orientation === "down") {
-        let prospectiveSlot = this.sea[y - turn][x];
+        let prospectiveSlot = this._sea[y + turn][x];
         turn++;
         return prospectiveSlot;
       } else if (orientation === "right") {
-        let prospectiveSlot = this.sea[y][x + turn];
+        let prospectiveSlot = this._sea[y][x + turn];
         turn++;
         return prospectiveSlot;
       } else if (orientation === "left") {
-        let prospectiveSlot = this.sea[y][x - turn];
+        let prospectiveSlot = this._sea[y][x - turn];
         turn++;
         return prospectiveSlot;
       }
@@ -119,31 +119,54 @@ export class Gameboard {
   }
 
   _addShip(y, x, orientation, length, type) {
+    //depending on the type of the ship it will be added to _boats in a specific index
+    switch (type) {
+      case "carrier":
+        this.boats[0] = new Ship(5, "carrier");
+        break;
+      case "battleship":
+        this.boats[1] = new Ship(4, "battleship");
+        break;
+      case "destroyer":
+        this.boats[2] = new Ship(3, "destroyer");
+        break;
+      case "submarine":
+        this.boats[3] = new Ship(3, "submarine");
+        break;
+      case "patrol boat":
+        this.boats[4] = new Ship(2, "patrol boat");
+        break;
+    }
     //variable to help us keep track of how many spots to take up for length of each ship
     let counter = 0;
-    this._boats.push(new Ship(length, type));
+    //add to ship to sea ... in an incrementing fashion
     while (counter < length) {
       if (orientation === "up") {
-        this.sea[y + counter][x] = this._boats.length;
+        this._sea[y - counter][x] =
+          this._boats.findIndex((boat) => boat.type === type) + 1;
         counter++;
       } else if (orientation === "down") {
-        this.sea[y - counter][x] = this._boats.length;
+        this._sea[y + counter][x] =
+          this._boats.findIndex((boat) => boat.type === type) + 1;
         counter++;
       } else if (orientation === "left") {
-        this.sea[y][x - counter] = this._boats.length;
+        this._sea[y][x - counter] =
+          this._boats.findIndex((boat) => boat.type === type) + 1;
         counter++;
       } else if (orientation === "right") {
-        this.sea[y][x + counter] = this._boats.length;
+        this._sea[y][x + counter] =
+          this._boats.findIndex((boat) => boat.type === type) + 1;
         counter++;
       }
     }
   }
 
   boatsRemaining() {
-    return this._boats.filter((boat) => !boat.sunk).length;
+    return this._boats.filter((boat) => boat.sunk === false).length;
   }
 
-  replaceShip(type, y, x, orientation) {
+  replaceShip(type, newY, newX, newOrientation) {
+    //type is partly how we choose between ships, when it comes down to replacing destroyer or submarine the order of boats in the array helps us because destroyer always precedes submarine
     let length =
       type === "carrier"
         ? 5
@@ -156,18 +179,66 @@ export class Gameboard {
               : type === "patrol boat"
                 ? 2
                 : null;
+    //temporary variable to hold old boats details, in case new boat won't fit where user is trying to place it, and old sea for if prospective new boat doesn't fit we can revert sea to previous state
+    let oldBoats = structuredClone(this._boats);
+    let oldSea = this._sea.slice();
 
-    //find the specific boat in the boats array
-    let targetedBoat = this._boats.findIndex((boat) => (boat.type = "type"));
-    //remove it
-    this._boats = this._boats
-      .slice(0, targetedBoat)
-      .concat(this._boats.slice(targetedBoat));
-    //replace it with new boat of same type
-    this.placeShip(y, x, orientation, length, type);
+    //find the specific preexisting boat in the boats array that will be replaced with this new boat
+    let targetedBoatIndex = this._boats.findIndex((boat) => boat.type === type);
+    //variable to store the value which represents the boat in the sea spots
+    let targetedBoatValue = targetedBoatIndex + 1;
+
+    //remove old boat data from _boats
+    this._boats[targetedBoatIndex] = "";
+
+    //wipe the sea of that old boat, use targetedBoatValue because index in the boats does not match natively with the value we use for the boats in the logic
+    this._sea = this._sea.map((row) =>
+      row.map(
+        (spot) =>
+          // eslint-disable-next-line prettier/prettier
+          spot === targetedBoatValue ? 0 : spot
+        // eslint-disable-next-line prettier/prettier
+      )
+    );
+
+    if (
+      //if boat passes vacancy checker of new position, and if it's within sea range...
+      this._vacancyChecker(newY, newX, newOrientation, length) &&
+      this._withinSeaRange(newY, newX)
+    ) {
+      //replace empty placeholder string in array with this new boat
+      this._boats[targetedBoatIndex] = new Ship(length, type);
+      //place new boat into sea using index of boat in boats array +1
+      let counter = 0;
+      while (counter < length) {
+        if (newOrientation === "up") {
+          this._sea[newY + counter][newX] = targetedBoatValue;
+        } else if (newOrientation === "down") {
+          this._sea[newY - counter][newX] = targetedBoatValue;
+        } else if (newOrientation === "left") {
+          this._sea[newY][newX - counter] = targetedBoatValue;
+        } else if (newOrientation === "right") {
+          this._sea[newY][newX + counter] = targetedBoatValue;
+          counter++;
+        }
+      }
+    } else {
+      //boat cant be placed at new position
+      //replace current boats with old boats
+      this._boats = structuredClone(oldBoats);
+      //revert sea
+      this._sea = oldSea.slice();
+
+      //some kind of error or return here
+      return false;
+    }
   }
 
   clearShips() {
-    this._boats = [];
+    this._boats = Array.from({ length: 5 }, () => "");
+    this._sea = Array.from({ length: 10 }, () =>
+      // eslint-disable-next-line prettier/prettier
+      Array.from({ length: 10 }, () => 0)
+    );
   }
 }
